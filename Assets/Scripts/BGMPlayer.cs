7e89141a1ff012e660c271cfac4568d74d266dc3
VinCMG <vincentchang9999@gmail.com>
2016-10-24 13:10:02 -0700

    Added temporary music to Assets/Audio
    Music won't be used in the actual game (unless Tina wants to or something I dunno)

--- Assets/Scripts/BGMPlayer.cs
+++ Assets/Scripts/BGMPlayer.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class BGMPlayer : MonoBehaviour {
 
+	public AudioSource audioSource;	
+	public AudioClip[] clipList;
+
 	// Use this for initialization
 	void Start () {
-	
+		audioSource = GetComponent<AudioSource>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
--- Assets/Scripts/TEMP_GameManager.cs
+++ Assets/Scripts/TEMP_GameManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class TEMP_GameManager : MonoBehaviour {
 
+	public BGMPlayer bgmPlayer;
+
 	// Use this for initialization
 	void Start () {
-	
+		bgmPlayer = GetComponent<BGMPlayer>();
 	}
 	
 	// Update is called once per frame
