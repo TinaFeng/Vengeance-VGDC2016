85b4c8d76ba080951bb2f2c741dc6efe6fd87991
VinCMG <vincentchang9999@gmail.com>
2016-10-25 13:17:17 -0700

    FadeOut and FadeIn implemented
    Some weird things you have to do to prevent them from overlapping if called too close to each other, but otherwise they should work well enough for now.

--- Assets/Scripts/BGMPlayer.cs
+++ Assets/Scripts/BGMPlayer.cs
@@ -6,6 +6,8 @@ public class BGMPlayer : MonoBehaviour {
 	public int numAudioSources = 2;
 	private AudioSource[] audioSources;
 	public BGMTrack track;
+	public float startingVolume = 1.0F;
+	private float mainVolume = 1.0F;
 
 	private double nextEventTime;
 	private int flip = 0;
@@ -34,7 +36,48 @@ public class BGMPlayer : MonoBehaviour {
 		}
 	}
 
-	public IEnumerator fadeOut(double seconds) {
-		return null;	
+	// NOTE: length of the fadein will not match lengthInSeconds exactly
+	public void fadeIn(float lengthInSeconds = 1.0F, float interval = 0.05F) {
+		Debug.Log(string.Format("Called fadeIn() with length {0} and interval {1}", lengthInSeconds, interval));
+		StartCoroutine(fadeInCoroutine(lengthInSeconds, interval));
 	}
+
+	// NOTE: length of the fadeout will not match lengthInSeconds exactly
+	public void fadeOut(float lengthInSeconds = 1.0F, float interval = 0.05F) {
+		Debug.Log(string.Format("Called fadeOut() with length {0} and interval {1}", lengthInSeconds, interval));
+		StartCoroutine(fadeOutCoroutine(lengthInSeconds, interval));
+	}
+
+	private IEnumerator fadeInCoroutine(float lengthInSeconds, float interval) {
+		double DEBUG_fadeoutTimer = AudioSettings.dspTime;
+		Debug.Log(string.Format("fadeOut() began at time {0}", DEBUG_fadeoutTimer));
+
+		while (mainVolume < startingVolume) {
+			foreach (var source in audioSources) {
+				source.volume += interval;
+			}
+			mainVolume += interval;		
+			yield return new WaitForSeconds(lengthInSeconds * interval);
+		}
+
+		DEBUG_fadeoutTimer -= AudioSettings.dspTime;
+		Debug.Log(string.Format("fadeOut() lasted {0} seconds", DEBUG_fadeoutTimer * -1));
+	}
+
+	private IEnumerator fadeOutCoroutine(float lengthInSeconds, float interval) {
+		double DEBUG_fadeoutTimer = AudioSettings.dspTime;
+		Debug.Log(string.Format("fadeOut() began at time {0}", DEBUG_fadeoutTimer));
+
+		while (mainVolume > 0.0F) {
+			foreach (var source in audioSources) {
+				source.volume -= interval;
+			}
+			mainVolume -= interval;		
+			yield return new WaitForSeconds(lengthInSeconds * interval);
+		}
+		
+		DEBUG_fadeoutTimer -= AudioSettings.dspTime;
+		Debug.Log(string.Format("fadeOut() lasted {0} seconds", DEBUG_fadeoutTimer * -1));
+	}
+
 }
--- Assets/Scripts/TEMP_GameManager.cs
+++ Assets/Scripts/TEMP_GameManager.cs
@@ -5,14 +5,41 @@ public class TEMP_GameManager : MonoBehaviour {
 
 	public BGMPlayer bgmPlayer;
 
+	private static bool DEBUG_fadeCoroutineCalled = false;
 	// Use this for initialization
 	void Start () {
-		bgmPlayer = GetComponent<BGMPlayer>();
+		bgmPlayer = GetComponentInChildren<BGMPlayer>();
 	}
-/*	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (!DEBUG_fadeCoroutineCalled) {
+			StartCoroutine(DEBUG_fadeTestCoroutine());
+			DEBUG_fadeCoroutineCalled = true;
+		}
 	}
-*/
+
+	private IEnumerator DEBUG_fadeTestCoroutine() {
+		DEBUG_waitThenFadeOutMusic();
+		yield return new WaitForSeconds(4.0F);
+		DEBUG_waitThenFadeInMusic();
+	}
+
+	void DEBUG_waitThenFadeInMusic() {
+		StartCoroutine(DEBUG_waitFadeIn());
+	}
+
+	void DEBUG_waitThenFadeOutMusic() {
+		StartCoroutine(DEBUG_waitFadeOut());
+	}
+
+	private IEnumerator DEBUG_waitFadeIn() {
+		yield return new WaitForSeconds(3.0F);
+		bgmPlayer.fadeIn(lengthInSeconds: 3.0F, interval: 0.05F);
+	}
+
+	private IEnumerator DEBUG_waitFadeOut() {
+		yield return new WaitForSeconds(3.0F);
+		bgmPlayer.fadeOut(lengthInSeconds: 3.0F, interval: 0.05F);
+	}
 }
