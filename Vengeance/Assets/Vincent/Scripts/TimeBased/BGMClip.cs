95ca5af7b50d12c65efb54912986e7534294539e
VinCMG <vincentchang9999@gmail.com>
2016-10-28 14:08:59 -0700

    Created TimeBased versions of audio players
    This way I can have tempo changes in the music and still be able to schedule clips at the correct time, instead of calculating the next time to schedule it based on the tempo and beats per clip.

---
+++ Vengeance/Assets/Vincent/Scripts/TimeBased/BGMClip.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMClip : MonoBehaviour {
+	public AudioClip clip;
+	public double lengthInSeconds;
+}
---
+++ Vengeance/Assets/Vincent/Scripts/TimeBased/TEMP_GameManager_TimeBased.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEMP_GameManager_TimeBased : MonoBehaviour {
+
+	public BGMPlayer_TimeBased bgmPlayer;
+
+	// Use this for initialization
+	void Start () {
+		bgmPlayer = GetComponentInChildren<BGMPlayer_TimeBased>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
