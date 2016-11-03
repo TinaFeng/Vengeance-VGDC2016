11583b87be14db2aeae68b9d0db3f167682dd8d9
VinCMG <vincentchang9999@gmail.com>
2016-11-03 16:34:50 -0700

    EnemySpawner can now spawn multiple types of enemies
    It looks for children that are tagged with the Enemy tag and adds them to a list of enemies that can be spawned. The List will probably be changed to a Dictionary in the future to make it easier when creating spawn sequences later.

--- Vengeance/Assets/Vincent/Scripts/EnemySpawner.cs
+++ Vengeance/Assets/Vincent/Scripts/EnemySpawner.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawner : MonoBehaviour {
 
-	public BaseEnemy enemyType; 
+	public List<GameObject> enemyTypes; 
 	private bool spawningStarted;
 	
 	// Use this for initialization
 	void Start () {
-		enemyType = GetComponentInChildren<BaseEnemy>();
+		foreach (Transform child in transform) {
+			if (child.tag == "Enemy") {
+				enemyTypes.Add(child.gameObject);
+			}
+		}
 		spawningStarted = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!spawningStarted) {
-			StartCoroutine(spawnEnemyOncePerSecondCoroutine());
+			startSpawning();
 			spawningStarted = true;
 		}
 	}
 
+	private void startSpawning() {
+		StartCoroutine(spawnEnemyOncePerSecondCoroutine());
+	}
+
 	private IEnumerator spawnEnemyOncePerSecondCoroutine() {
+		int flip = 1;
 		for (int i = 0; i < 3; ++i) {
-			Instantiate(enemyType, transform.position, transform.rotation);
+			Instantiate(enemyTypes[flip], transform.position, transform.rotation);
 			Debug.Log("Spawned enemy " + i);
+			flip = 1 - flip;
 			yield return new WaitForSeconds(1.0f);
 		}
 	}
