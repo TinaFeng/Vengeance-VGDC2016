821dbde7b07bba543a9d362c0ff83ae8df14478e
Ittaimann <ittaiam@gmail.com>
2016-11-11 13:35:41 -0800

    Ai movement
    only movement, no destruction, shooting, or player tracking

---
+++ Vengeance/Assets/Ittai/scripts/Enemy_movement_2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_movement_2 : MonoBehaviour {
+    private Rigidbody2D RB;
+
+    public float x_coor;
+    public float y_coor;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+
+        RB = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 origin = new Vector2(RB.transform.position.x,RB.transform.position.y);
+        if (RB.transform.position!= new Vector3(x_coor, y_coor,0) );
+        {
+            RB.MovePosition(Vector2.MoveTowards(origin, new Vector2(x_coor, y_coor), Time.deltaTime*speed));
+
+        }
+	}
+}
