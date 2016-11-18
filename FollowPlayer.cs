bcd1ce27d2259e9d910f46a3c7a497f62dfb3293
cjgonza3 <cjgonza3@uci.edu>
2016-11-18 11:40:12 -0800

    level 1 bullet movement scripts

---
+++ FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public string player = "PlayerShip";
+    public float speed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rot = transform.rotation;
+        Vector3 dir = transform.position - GameObject.Find(player).transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rot = Quaternion.Euler(0, 0, angle + 90.0f);
+        transform.rotation = rot;
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, speed * Time.deltaTime, 0);
+        pos += rot * velocity;
+        transform.position = pos;
+    }
+}
---
+++ SpawnPerpendicular.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPerpendicular : MonoBehaviour {
+
+    public GameObject bullet;
+    public float delay = 0.5f;
+    float timer = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = delay;
+            Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 90));
+            Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 270));
+        }
+    }
+}
