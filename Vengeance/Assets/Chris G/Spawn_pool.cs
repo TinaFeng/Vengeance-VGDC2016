68039f18bca4171c909a66f04f9452887dd503f2
cjgonza3 <cjgonza3@uci.edu>
2016-11-18 13:33:49 -0800

    Spawners shoot straight and curved bullets

---
+++ Vengeance/Assets/Chris G/Spawn_pool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_pool : MonoBehaviour {
+
+    public float spawnTime = 5f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+        GameObject obj = bullet_pool.current.GetBullet();
+
+        if (obj == null) return;
+
+        obj.transform.position = transform.position;
+        obj.transform.rotation = transform.rotation;
+        obj.SetActive(true);
+	}
+}
