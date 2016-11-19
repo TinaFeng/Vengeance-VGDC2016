978ab2c1dde82d7f565feecd97bd9746a7b55690
DGunn <dgunn@trashmail.com>
2016-11-18 17:22:47 -0800

    Character bullet framework (beginning skeleton)
    The work produced during today's team meeting. Synced in order to gain
    assets from current commit.

--- Vengeance/Assets/Devon/Scripts/CharacterMovement.cs
+++ Vengeance/Assets/Devon/Scripts/CharacterMovement.cs
@@ -3,20 +3,53 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
 
+    //public variables
     public float speed;
+
+    //private variables
     private Vector2 movement;
+    private Vector2 halfPlayerDimensions;
+    private Vector2 cameraDimensions;
+
     private Rigidbody2D rb2d;
+
     private SpriteRenderer charRenderer;
+
     private GameObject myObject;
+
+    //z = fire; x = bomb; c = cycle through abilities
+    public enum myAttackKeys {FIRE = 'Z', BOMB = 'X', CYCLE = 'C'};
 
 	// Use this for initialization
 	void Start () {
+        //initialize movement to a vector2(x,y)
         movement = new Vector2();
+
+        //pass the Rigidbody2D object associated with our player into rb2d memory space
         rb2d = GetComponent<Rigidbody2D>();
+
+        //pass the HitDot (child of player) game object to the myObject memory space
         myObject = GameObject.Find("HitDot");
+
+        //pass the SpriteRenderer component of the HitDot child object, and pass it to the charRenderer memory space
         charRenderer = myObject.GetComponent<SpriteRenderer>();
+        //disable the dot so that it only shows up when keys are depressed
         charRenderer.enabled = false;
-	}
+
+
+        //capture half the player's width and height for calculating collsion with camera frustum
+        //Sprite tempSprite = charRenderer.sprite;
+        Vector4 tempBorder = charRenderer.sprite.border;
+        //print("X: " + tempBorder.x + ", Y: " + tempBorder.y);
+
+        GameObject tempCamera = GameObject.Find("Main Camera");
+
+        //capture camera's width and height
+        //cameraDimensions.x
+        //cameraDimensions.y = Camera.current.rect.height;
+
+        print("X: " + cameraDimensions.x + ", Y: " + cameraDimensions.y);
+    }
 	
     void Update()
     {
@@ -35,10 +68,14 @@ public class CharacterMovement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+        //get player's inputs
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        //apply them to the character
         rb2d.velocity = movement * speed;
+
+        //check to see if the player is out of bounds, and if so, keep them in bounds
 
 	}
 }
