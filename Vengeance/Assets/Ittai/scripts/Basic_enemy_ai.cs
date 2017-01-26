9d83991a7dcc0358b217121b7af502a66e4ceff3
Ittai Mann <manni@uci.edu>
2017-01-25 18:57:10 -0800

    small changes to enemy ai, changeed prefab
    Changed player bullet to use playerBullet tag. Also starting to change
    certain things about how the enemy ai works. enemy now dies.

--- Vengeance/Assets/Ittai/scripts/Basic_enemy_ai.cs
+++ Vengeance/Assets/Ittai/scripts/Basic_enemy_ai.cs
@@ -16,36 +16,10 @@ public class Basic_enemy_ai : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        RB=GetComponent<Rigidbody2D>();
 	}
 
 
     void FixedUpdate() {
-        Vector2 Move_direction;
 
-
-        if (direction == Directions.horizontal)
-            Move_direction = new Vector2(speed, 0);
-        else
-            Move_direction = new Vector2(0, -speed); // negative bc coordinate system I guess
-        
-
-        if (RB.transform.position.x <= xbounds[0] || RB.transform.position.x >= xbounds[1])
-        {
-            RB.transform.position = new Vector2(Mathf.Clamp(RB.transform.position.x, 
-                    (xbounds[0]+.1f), (xbounds[1]-.1f)), RB.transform.position.y);// clamps the transform to the boundaries, makes sure that it won't move beyond those and then
-                                                                                  //sets it back into place
-            speed *= -1; //reverse
-        }
-
-        if (RB.transform.position.y>= ybounds[0] || RB.transform.position.y <= ybounds[1])
-        {
-            RB.transform.position = new Vector2(RB.transform.position.x, Mathf.Clamp(RB.transform.position.y,
-                    (ybounds[1] + .1f), (ybounds[0] - .1f)));// clamps the transform to the boundaries, makes sure that it won't move beyond those and then
-                                                                                     //sets it back into place
-            speed *= -1;
-        }
-
-        RB.MovePosition(RB.position+Move_direction * Time.deltaTime);
 	}
 }
---
+++ Vengeance/Assets/Ittai/scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    public int health=50;   //public health with the base being 50, changeable in editor
+    public string movement;     // what kind of enemy this is, this will dictate how the enemy moves and which bullets the enemy will instantiate in a different script attached to all enemys
+    public string bullets;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "playerBullet")
+        {
+            health -= 10;
+        }
+    }
+}
