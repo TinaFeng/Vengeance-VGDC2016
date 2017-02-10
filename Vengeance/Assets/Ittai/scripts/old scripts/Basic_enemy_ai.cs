bc9bc5fa9b4ca85cb1583d97972f8afd1844ee1a
Christopher Peterson <cjpeter1@uci.edu>
2017-02-09 18:38:47 -0800

    Revert "Added new File structure"
    This reverts commit bcb86c6c8539cbb1e8021eaf4d5d01752fb9e75a.

---
+++ Vengeance/Assets/Ittai/scripts/old scripts/Basic_enemy_ai.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basic_enemy_ai : MonoBehaviour {
+
+    private Rigidbody2D RB;
+
+    public float[] xbounds; //int 0= left boundm int 1= right bound, 
+    public float[] ybounds; //int 0 = upper bound, int 1 = lower bound
+
+    public int LIFE=50;
+    public int speed;
+
+    public enum Directions {horizontal, vertical};
+    public Directions direction;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+
+    void FixedUpdate() {
+
+	}
+}
