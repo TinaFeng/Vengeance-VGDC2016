dd81f51d20da35097c94e0351ca2cc408d99511c
TinaFeng <xuanfeng1996@gmail.com>
2017-05-27 00:28:10 -0700

    Boss Tracker(Indicator) works

---
+++ Vengeance/Assets/Boss_Tracker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Tracker : MonoBehaviour {
+
+    private GameObject Boss;
+    private float Boss_Position;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().enabled = false;
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Boss = GameObject.FindGameObjectWithTag("Boss");
+        
+        if (Boss == null)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+
+            Boss_Position = Boss.GetComponent<Rigidbody2D>().position.x;
+            this.transform.position = new Vector3(Boss_Position, this.transform.position.y, this.transform.position.z);
+           
+        }
+    }
+}
