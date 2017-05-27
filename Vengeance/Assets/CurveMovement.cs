64e997c97f9185f7314af63fbf754029d41fe58e
TinaFeng <xuanfeng1996@gmail.com>
2017-05-26 22:47:30 -0700

    Full Animation Implemented

--- Vengeance/Assets/CurveMovement.cs
+++ Vengeance/Assets/CurveMovement.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 
 public class CurveMovement : MonoBehaviour {
 
+    
     public AnimationCurve locX;
     public AnimationCurve locY;
     public float timeToKill = 0;
     float time = 0;
+    Vector3 prev;
     Vector3 temp;
-        
+
+    public Animator anim;
+
+    void Start()
+    {
+
+       prev.Set(locX.Evaluate(time), locY.Evaluate(time), 0f);
+        anim = GetComponent<Animator>();
+    }
 	void FixedUpdate () {
         time += Time.deltaTime;
+        
         temp.Set(locX.Evaluate(time), locY.Evaluate(time), 0f);
         transform.position = temp;
-        if(time > timeToKill && timeToKill != 0)
+        /////Determine direction between updates
+        
+        if (temp.x - prev.x > 0)
+            anim.SetBool("Right", true);
+   
+      
+        else if (temp.x - prev.x < 0)
+            anim.SetBool("Left", true);
+ 
+        else
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", false);
+        }
+        
+        prev = temp;
+        if (time > timeToKill && timeToKill != 0)
         {
             gameObject.SetActive(false);
         }
