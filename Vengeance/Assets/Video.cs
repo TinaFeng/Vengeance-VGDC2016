a13df3eba6e74496ad884ed6cd8ad009e1ed08b5
TinaFeng <xuanfeng1996@gmail.com>
2017-05-30 15:17:01 -0700

    Trying craps

---
+++ Vengeance/Assets/Video.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
