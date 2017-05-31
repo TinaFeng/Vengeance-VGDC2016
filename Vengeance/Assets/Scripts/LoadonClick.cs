0a5f7ad1aab584c71fae177cc7c370df6f45bed8
TinaFeng <xuanfeng1996@gmail.com>
2017-05-31 16:21:10 -0700

    Basic Menu Complete

---
+++ Vengeance/Assets/Scripts/LoadonClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadonClick : MonoBehaviour {
+
+
+   public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene); 
+    }
+}
