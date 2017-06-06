45a85bcc92f38f14d55f9e59374ff1b22277a170
TinaFeng <xuanfeng1996@gmail.com>
2017-06-06 00:28:08 -0700

    Finished Particle System

--- Vengeance/Assets/EnemyStats.cs
+++ Vengeance/Assets/EnemyStats.cs
@@ -5,21 +5,50 @@ using UnityEngine;
 public class EnemyStats : MonoBehaviour {
 
     GameObject player;
+	ParticleSystem deatheffect;
     public int health = 50;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+		deatheffect = GetComponent<ParticleSystem> ();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         other.gameObject.SetActive(false);
         health -= 10;
+
+
         if (health <= 0)
-        {
+		{
+			StartCoroutine(PaticleTime ());
             player.GetComponent<PlayerController>().score += 100;
-            gameObject.SetActive(false);
+
+            
         }
     }
+
+	IEnumerator PaticleTime()
+
+	{	
+		if (gameObject.tag == "Boss") {
+			
+			deatheffect.Play ();
+			deatheffect.Play ();
+			deatheffect.Play ();
+			this.GetComponent<SpriteRenderer> ().enabled = false;
+			yield return new WaitForSeconds (2f);
+		} 
+		else {
+			this.GetComponent<SpriteRenderer> ().enabled = false;
+			deatheffect.Play ();
+		
+			yield return new WaitForSeconds (0.2f);
+
+		}
+
+
+		gameObject.SetActive(false);
+	}
 }
