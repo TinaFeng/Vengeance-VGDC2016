3a32242ef299076b94845aa5f31ca5edd8848dc1
Unknown <DJGunn@uci.edu>
2017-06-27 13:57:33 -0700

    Dialogue XML Update 1
    Added initial dialogue XML file, did some code rearrangement, and began skeleton work of the C# XML code.

---
+++ Vengeance/Assets/Scripts/Dialogue/DialogueContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("DialogueCollection")] //points to the root of our XML tree, which represents a collection of dialogue
+
+public class DialogueContainer {
+
+    [XmlArray("LevelDialogue")]
+    [XmlArrayItem("Dialogue")]
+    public List<Dialogue> levelDialogue = new List<Dialogue>();
+}
