942a2329708324e2f90f993a73a775939585d7c6
Christopher Peterson <cjpeter1@uci.edu>
2017-07-15 23:51:12 -0700

    Adjusted Fade Script

---
+++ Vengeance/Assets/SceneStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<ScreenFade>().fadeIn(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Vengeance/Assets/ScreenFade.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFade : MonoBehaviour {
+
+    public Image image;
+    float t;
+    float time;
+    bool fade = true;
+
+    // Update is called once per frame
+    void Update () {
+        if (fade)
+        {
+            t += Time.deltaTime / time;
+            Debug.Log(t);
+            image = GetComponent<Image>();
+            var tempColor = image.color;
+            tempColor.a = Mathf.Lerp(0, 1, t);
+            image.color = tempColor;
+            if (t < 0 || t > 1)
+                fade = false;
+        }
+    }
+
+    public void fadeIn(float x)
+    {
+        time = -x;
+        t = 1;
+        image.enabled = true;
+        fade = true;
+    }
+
+    public void fadeOut(float x)
+    {
+        time = x;
+        t = 0;
+        image.enabled = true;
+        fade = true;
+    }
+}
