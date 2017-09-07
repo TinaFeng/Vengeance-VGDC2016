9e09740ed25fad197a1004d74472aae3e9ff109d
Christopher Peterson <cjpeter1@uci.edu>
2017-09-07 15:51:42 -0700

    Enabled Multi-Object Editing

--- Vengeance/Assets/Editor/CurveMovementViewer.cs
+++ Vengeance/Assets/Editor/CurveMovementViewer.cs
@@ -12,8 +12,13 @@ public class CurveMovementViewer: Editor {
 	public void OnSceneGUI()
     {
         var script = target as CurveMovement;
-        positions = new Vector3[(int)script.timeToKill*10];
-        for(int i = 0; i < (int)script.timeToKill * 10; i++)
+        int size;
+        if(script.timeToKill != 0)
+            size = (int)script.timeToKill*10;
+        else
+            size = 1000;
+        positions = new Vector3[size];
+        for (int i = 0; i < size; i++)
         {
             positions[i] = new Vector3(script.locX.Evaluate(i * 0.1f), script.locY.Evaluate(i * 0.1f), 0f);
         }
--- Vengeance/Assets/Editor/SpawnBulletEditor.cs
+++ Vengeance/Assets/Editor/SpawnBulletEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+[CanEditMultipleObjects]
 [CustomEditor(typeof(SpawnBullet))]
 public class SpawnBulletEditor : Editor
 {
